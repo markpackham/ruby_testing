e5d039613bf636725b484cb89c67b1911a13258c
mark <markpackham1@gmail.com>
2020-03-31 17:10:21 +0100

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,51 @@
+def return_10()
+    return 10;
+end
+
+def add(a,b)
+    return a + b
+end
+
+def subtract(a,b)
+    return a - b
+end
+
+def multiply(a,b)
+    return a * b 
+end
+
+def divide(a,b)
+    return a / b 
+end
+
+def length_of_string(a)
+    return a.length()
+end
+
+def join_string(a,b)
+    return a+b
+end
+
+def add_string_as_number(a,b)
+    return a.to_i + b.to_i
+end
+
+def number_to_full_month_name(a)
+    arr = ["January", "Febrauary", "March", "April", "May", "June", "July", "August", "September", "October", "Novermber", "December"]
+    return arr[a - 1]
+end
+
+def number_to_short_month_name(a)
+    arr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    return arr[a - 1]
+end
+
+def volume_of_cube(a)
+    return a**3
+end
+
+# T(°C) = (68°F - 32) × 5/9 = 20 °C
+# 50 = 10
+def f_c(a)
+return (a - 32) * 5/9
+end
